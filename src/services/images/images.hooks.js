3ce6e1a4fdf013ee145a47d352f3c1d376a8b4fb
Olaini Lature <olature@hiapp.id>
2022-04-13 05:46:11 +0700

    add method find for images of flickr

--- src/services/images/images.hooks.js
+++ src/services/images/images.hooks.js
@@ -1,9 +1,41 @@
-
+const axios = require('axios').default;
+const errors = require('@feathersjs/errors');
 
 module.exports = {
   before: {
     all: [],
-    find: [],
+    find: [
+      async context => {
+        const {
+          app,
+          params
+        } = context;
+        const {
+          query
+        } = params;
+
+        const FLICKR_URL = app.get('flickr-url');
+
+        let tags = '';
+
+        if (query.tags !== undefined) {
+          tags = '&tags=' + JSON.parse(JSON.stringify(query.tags));
+        }
+
+        await axios.get(FLICKR_URL + '?format=json' + tags).then((res) => {
+          let response = res.data.substring(15, res.data.length);
+          response = response.substring(0, response.length - 1);
+          response = JSON.parse(response);
+          context.result = response;
+        }).catch((err) => {
+          throw new errors.Unprocessable('Flickr Server not response', {
+            error: err
+          });
+        });
+
+        return context;
+      }
+    ],
     get: [],
     create: [],
     update: [],
